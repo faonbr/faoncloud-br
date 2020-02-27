ed561d7df19fcdfc8294e7f5e811e272254c93f3
FAONbr <faonbr@gmail.com>
2020-02-27 19:22:40 -0300

    min max ajustments

--- config/modules.js
+++ config/modules.js
@@ -41,8 +41,8 @@ export const modulesSettings = {
   responsiveLoader: {
     name: 'uploads/[hash:7]-[width].[ext]',
     min: 320, // minimum image width generated (default 640)
-    max: 900, // maximum image width generated (default 1800)
-    steps: 4, // five sizes per image will be generated
+    max: 640, // maximum image width generated (default 1800)
+    steps: 3, // five sizes per image will be generated (default 4)
     placeholder: false, // no placeholder will be generated
     quality: 65, // images are compressed with medium quality
     adapter: require('responsive-loader/sharp'),
