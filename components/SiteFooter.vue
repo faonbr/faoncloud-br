3d9671f8ccad3d0ab0fa6da71aada33db5c5c219
FAONbr <faonbr@gmail.com>
2020-02-27 01:49:18 -0300

    a

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,8 +1,9 @@
 <template>
   <footer class="site-footer">
-    {{ $siteConfig.siteName }} &copy; {{ new Date().getFullYear() }} - Direitos Reservados
+    {{ $siteConfig.siteName }} &copy; {{ new Date().getFullYear() }} - All
+    Rights Reserved
     <br />
-    Construído com <span style="color: #e25555;">&#9829;</span> com Nuxt.js e
+    Made with <span style="color: #e25555;">&#9829;</span> with Nuxt.js and
     Netlify
   </footer>
 </template>
